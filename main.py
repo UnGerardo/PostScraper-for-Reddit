bc73226c99f52b22307a46ead87cceca009f2ca5
UnGerardo <gerardojdls@gmail.com>
2020-10-21 20:50:56 -0700

    Added functionality to input a key word and save it to a file

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 import praw
 import tkinter as tk
-from tkinter import filedialog, Text
+from tkinter import Text
 import os
 import api_keys as key
 
 
-def add_key_words(key_word):
-    key_word_list.append(key_word)
+def add_key_words():
+    # this gets the text input in the text box
+    key_word = new_key_word_txt.get("1.0", 'end-1c')
+    # if the text isn't empty add it to the key word list
+    if key_word:
+        key_word_list.append(key_word)
 
 
 def get_posts():
@@ -64,6 +68,9 @@ if __name__ == '__main__':
     add_key_word_btn = tk.Button(root, text='Add Key Word', padx=10,  # create button to add key words
                                  pady=5, fg='white', bg='#263D42', command=add_key_words)
     add_key_word_btn.pack()  # attach button to root but its not attached to the canvas or frame
+
+    new_key_word_txt = tk.Text(root, width=10, height=1) # creates text box to write in
+    new_key_word_txt.pack() # displays text box
 
     get_posts_btn = tk.Button(root, text='Get Posts', padx=10,  # create button to get posts
                               pady=5, fg='white', bg='#263D42', command=get_posts)
