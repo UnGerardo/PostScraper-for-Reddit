06e7e5c00d4210509300f378fec966e9e47851f4
UnGerardo <gerardojdls@gmail.com>
2020-10-22 18:24:23 -0700

    Added scrollability

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import praw
 import tkinter as tk
-from tkinter import Text
+from tkinter import ttk
 import os
 import api_keys as key
 
@@ -15,19 +15,18 @@ def add_key_words():
 
 def get_posts():
     # clears the frame
-    for widget in frame.winfo_children():
+    for widget in scrollable_frame.winfo_children():
         widget.destroy()
 
     # gets new posts in each sub and searches for keywords in the titles
     try:
         for sub in subscribed:
-            for post in reddit.subreddit(sub.title).new(limit=10):
+            for post in reddit.subreddit(sub.title).new(limit=50):
                 for word in key_word_list:
                     if word in post.title.lower():
-                        post_title = tk.Label(frame, text=post.title, bg='gray')
-                        post_title.pack()
-                        post_url = tk.Label(frame, text="https://www.reddit.com" + post.permalink, bg='gray')
-                        post_url.pack()
+                        tk.Label(scrollable_frame, text=post.title).pack()
+                        post_link = tk.Label(scrollable_frame, text="https://www.reddit.com" + post.permalink, fg="blue", cursor="hand2")
+                        post_link.pack()
                         # print(post.title, "https://www.reddit.com" + post.permalink)
     except:
         print('Ran into an error getting posts')
@@ -54,26 +53,44 @@ if __name__ == '__main__':
     # get subreddits the user follows
     subscribed = list(reddit.user.subreddits(limit=None))
 
-    # root is the body of the gui
-    root = tk.Tk()
+    # root is the body of the gui, name needs space as first char because it goes lowercase
+    root = tk.Tk(className=' Post Scrapper')
+    # sets the window size at the size inside 'widthxheight'
+    root.geometry("500x500")
 
-    canvas = tk.Canvas(root, height=500, width=500, bg='#263D42')
-    # attaches canvas to the root
-    canvas.pack()
+    # allows you to frame widgets bg='#16697A'
+    container = ttk.Frame(root)
+    canvas = tk.Canvas(container, width=800)
+    scrollbar = ttk.Scrollbar(container, orient="vertical", command=canvas.yview)
+    scrollable_frame = ttk.Frame(canvas)
 
-    # interface within canvas (like a div)
-    frame = tk.Frame(root, bg='white')
-    frame.place(relwidth=0.8, relheight=0.8, relx=0.1, rely=0.1)
+    scrollable_frame.bind(
+        "<Configure>",
+        lambda e: canvas.configure(
+            scrollregion=canvas.bbox("all")
+        )
+    )
 
+    canvas.create_window((0, 0), window=scrollable_frame, anchor="nw")
+
+    canvas.configure(yscrollcommand=scrollbar.set)
+
+    container.pack()
+    canvas.pack(side="left", fill="both", expand=True)
+    scrollbar.pack(side="right", fill="y")
+
+    # text to label key word input
+    add_key_word_txt = tk.Label(root, text='Key Word')
+    add_key_word_txt.pack()
+    add_key_word_txt.place(relx=0.5, rely=0.5, anchor='center')
     add_key_word_btn = tk.Button(root, text='Add Key Word', padx=10,  # create button to add key words
-                                 pady=5, fg='white', bg='#263D42', command=add_key_words)
+                                 pady=5, fg='white', bg='#DB6400', command=add_key_words)
     add_key_word_btn.pack()  # attach button to root but its not attached to the canvas or frame
-
     new_key_word_txt = tk.Text(root, width=10, height=1) # creates text box to write in
-    new_key_word_txt.pack() # displays text box
+    new_key_word_txt.pack()  # displays text box
 
     get_posts_btn = tk.Button(root, text='Get Posts', padx=10,  # create button to get posts
-                              pady=5, fg='white', bg='#263D42', command=get_posts)
+                              pady=5, fg='white', bg='#DB6400', command=get_posts)
     get_posts_btn.pack()  # attach button to root but its not attached to the canvas or frame
 
     # runs the gui
